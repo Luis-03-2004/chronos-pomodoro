fac1adef6ab64db3a65612e934bb3ea45545b39a
Luis-03-02004 <luisfelipe.monteiroferreira@gmail.com>
2026-07-17 16:18:17 -0300

    style: using and importing css modules with component scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Heading } from "./components/Heading";
+
 import "./styles/theme.css";
 import "./styles/global.css";
 
@@ -5,7 +7,7 @@ export function App() {
   console.log("Opa");
   return (
     <div>
-      <h1>Olá Mundo</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, atque
         vitae, aspernatur expedita iusto exercitationem qui tempore
